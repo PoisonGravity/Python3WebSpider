95fd0b8c808521e22f6c9a881a6910ac7cec5d3c
Lucidity <2993514952@qq.com>
2022-06-15 18:48:14 +0800

    add some code

--- Chapter2/2.2 requests的使用/requests的使用.py
+++ Chapter2/2.2 requests的使用/requests的使用.py
@@ -100,11 +100,38 @@ requests
 #     print(key + "=" + value)
 
 # 例子2：
+# import requests
+#
+# headers = {
+#     'cookie': '_octo=GH1.1.2133112272.1649413925; tz=Asia%2FShanghai; _device_id=3f76259ba6a9560b3c09c8ebbc19e0a7; user_session=0KW9eFkKJYzIP8o0OOmo7tRC_JFa_3lUl0I872xVkA1mkozv; __Host-user_session_same_site=0KW9eFkKJYzIP8o0OOmo7tRC_JFa_3lUl0I872xVkA1mkozv; tz=Asia%2FShanghai; color_mode=%7B%22color_mode%22%3A%22dark%22%2C%22light_theme%22%3A%7B%22name%22%3A%22light%22%2C%22color_mode%22%3A%22light%22%7D%2C%22dark_theme%22%3A%7B%22name%22%3A%22dark%22%2C%22color_mode%22%3A%22dark%22%7D%7D; logged_in=yes; dotcom_user=Lucid1ty; has_recent_activity=1; _gh_sess=323MBuo3IWPPXhEnq9x5A%2FT%2BjRD00J4Y057K1NsmhYoIDg1apw2NgHs%2BgRiPbFwXIaql7E2ZJlm0OlsyROBXx5BIrEGJi2R6wDnQ8NsA5tepgkLY4NkZ4xKaPE%2FOJtsu1ya9AgF4lCl12QTCbc%2B75IDLuEo8XABnbUhCmZD%2BE6FyrhV9KTOEgVJQ6IK36j99AY7eFatJNC2ExohIHi6BYCKh55WA5voYTr4zd2et3RO4vAqX6fvVqEeL3Dc4x2xoMaDWQvSinsdOkNMDAGsIovHPW7%2FQiDV1C1Fy3A%3D%3D--Pv8pjirHpmQDEsrZ--NmaBaQjJWDNzEjz7ht7ESw%3D%3D',
+#     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
+# }
+# r = requests.get('https://github.com', headers=headers)
+# print(r.text)
+
+
+# Session 维持
+"""
+场景：
+第一个请求利用 requests 库的 post 方法登录了某个网站，第二次想要获取成功登录后的自己的个人信息
+于是又用了一次 requests 库的 get 方法去请求个人页面信息
+这实际上相当于打开了两个浏览器，是互相独立的，对应完全不相关的 Session
+那么第二次的请求就获取不到个人信息
+解决办法：Session对象
+"""
+# 沿用之前的写法：
+# import requests
+#
+# requests.get('https://www.httpbin.org/cookies/set/number/123456789')
+# r = requests.get('https://www.httpbin.org/cookies')
+# print(r.text)   # 得到的 cookies 是空的
+
+
+# 利用 Session
 import requests
 
-headers = {
-    'cookie': '_octo=GH1.1.2133112272.1649413925; tz=Asia%2FShanghai; _device_id=3f76259ba6a9560b3c09c8ebbc19e0a7; user_session=0KW9eFkKJYzIP8o0OOmo7tRC_JFa_3lUl0I872xVkA1mkozv; __Host-user_session_same_site=0KW9eFkKJYzIP8o0OOmo7tRC_JFa_3lUl0I872xVkA1mkozv; tz=Asia%2FShanghai; color_mode=%7B%22color_mode%22%3A%22dark%22%2C%22light_theme%22%3A%7B%22name%22%3A%22light%22%2C%22color_mode%22%3A%22light%22%7D%2C%22dark_theme%22%3A%7B%22name%22%3A%22dark%22%2C%22color_mode%22%3A%22dark%22%7D%7D; logged_in=yes; dotcom_user=Lucid1ty; has_recent_activity=1; _gh_sess=323MBuo3IWPPXhEnq9x5A%2FT%2BjRD00J4Y057K1NsmhYoIDg1apw2NgHs%2BgRiPbFwXIaql7E2ZJlm0OlsyROBXx5BIrEGJi2R6wDnQ8NsA5tepgkLY4NkZ4xKaPE%2FOJtsu1ya9AgF4lCl12QTCbc%2B75IDLuEo8XABnbUhCmZD%2BE6FyrhV9KTOEgVJQ6IK36j99AY7eFatJNC2ExohIHi6BYCKh55WA5voYTr4zd2et3RO4vAqX6fvVqEeL3Dc4x2xoMaDWQvSinsdOkNMDAGsIovHPW7%2FQiDV1C1Fy3A%3D%3D--Pv8pjirHpmQDEsrZ--NmaBaQjJWDNzEjz7ht7ESw%3D%3D',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
-}
-r = requests.get('https://github.com', headers=headers)
-print(r.text)
+s = requests.Session()
+s.get('https://www.httpbin.org/cookies/set/number/123456789')
+r = s.get('https://www.httpbin.org/cookies')
+print(r.text)   # Cookie 被成功获取！
+
