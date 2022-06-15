2e4b1dc79cf72e79e039f0a659e1f2d6619ff290
Lucidity <2993514952@qq.com>
2022-06-15 15:56:25 +0800

    add some code

--- Chapter2/2.1 urllib的使用/requests的使用.py
+++ Chapter2/2.1 urllib的使用/requests的使用.py
@@ -11,12 +11,17 @@ requests
 在处理网页验证和 Cookie 时，需要写 Opener 类和 Handler 类来处理
 另外实现 POST 和 GET 请求的写法也不太方便
 为了更方便的实现这些操作，产生了更强大的库：requests
-pip3 install requests
+安装：pip3 install requests
 """
 # 例子：
 import requests
 
 r = requests.get('https://www.baidu.com/')
+# r = requests.post('https://www.httpbin.org/get')
+# r = requests.post('https://www.httpbin.org/post')
+# r = requests.post('https://www.httpbin.org/put')
+# r = requests.post('https://www.httpbin.org/delete')
+# r = requests.post('https://www.httpbin.org/patch')
 print(type(r))
 print(r.status_code)
 print(type(r.text))
