e6723c5b75e2accf58bfcadb472a192c491b0348
Lucidity <2993514952@qq.com>
2022-06-15 14:29:33 +0800

    add some code

--- Chapter2/2.1 urllib的使用/登录验证网站.py
+++ Chapter2/2.1 urllib的使用/登录验证网站.py
@@ -3,8 +3,23 @@
 # @Author : Cosmica
 # @File : 登录验证网站
 # @Project : Spider
+# 遇到要登录的网站可能会弹出认证窗口让你输入用户名和密码登录
+# 这种情况借助 HTTPBasicAuthHandler 模块就可以完成，代码如下
 from urllib.request import HTTPPasswordMgrWithDefaultRealm, HTTPBasicAuthHandler, build_opener
 from urllib.error import URLError
 
 username = 'admin'
 password = 'admin'
+url = 'https://ssr3.scrape.center/'
+
+p = HTTPPasswordMgrWithDefaultRealm()
+p.add_password(None, url, username, password)
+auth_handler = HTTPBasicAuthHandler(p)
+opener = build_opener(auth_handler)
+
+try:
+    result = opener.open(url)
+    html = result.read().decode('utf-8')
+    print(html)
+except URLError as e:
+    print(e.reason)
