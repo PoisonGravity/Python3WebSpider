866a74752f3dd0b2691bb1b4d2f4ba4ae9aec33d
Lucidity <2993514952@qq.com>
2022-06-15 20:23:54 +0800

    add some code

--- Chapter2/2.3 正则表达式/匹配目标并提取.py
+++ Chapter2/2.3 正则表达式/匹配目标并提取.py
@@ -3,12 +3,38 @@
 # @Author : Cosmica
 # @File : 匹配目标并提取
 # @Project : Spider
+
+
+# P66
+# import re
+#
+# content = 'Hello 1234567 World_This is a Regex Demo'
+# result = re.match('^Hello\s(\d+)\sWorld', content)
+# print(result)
+# print(result.group())
+# # 调用 group 方法并传入索引，提取要匹配的目标
+# print(result.group(1))  # group(1) 表示输出第一个被()匹配的结果
+# print(result.span())
+
+
+# P67
+# 通用匹配：.* 匹配任意字符(可以简化正则表达式的书写)
+# import re
+#
+# content = 'Hello 123 4567 World_This is a Regex Demo'
+# result = re.match('^Hello.*Demo$', content)
+# print(result)
+# print(result.group())
+# print(result.span())
+
+
+# P67
+# 贪婪与非贪婪
 import re
 
 content = 'Hello 1234567 World_This is a Regex Demo'
-result = re.match('^Hello\s(\d+)\sWorld', content)
+result = re.match('^He.*(\d+).*Demo$', content)
 print(result)
-print(result.group())
-# 调用 group 方法并传入索引，提取要匹配的目标
 print(result.group(1))
-print(result.span())
+
+
