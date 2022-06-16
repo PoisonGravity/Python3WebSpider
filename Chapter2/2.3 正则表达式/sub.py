06026f1ede9908c1a971a334f7d7204933b64e1b
Lucidity <2993514952@qq.com>
2022-06-16 17:02:04 +0800

    add some code

--- Chapter2/2.3 正则表达式/sub.py
+++ Chapter2/2.3 正则表达式/sub.py
@@ -3,3 +3,12 @@
 # @Author : Cosmica
 # @File : sub
 # @Project : Spider
+
+# P71
+# sub
+import re
+
+content = '54aK54yr5oiR54ix5L2g'
+# 第一个参数传入正则表达式匹配要替换的内容，第二个参数为要替换的内容，第三个参数为原字符串
+content = re.sub('\d+', '', content)
+print(content)  # 得到 aKyroiRixLg，这是去除了数字的结果
