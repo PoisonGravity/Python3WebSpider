1d29269d98318b491fe95fb9ec378d452b46284f
Lucidity <2993514952@qq.com>
2022-06-16 17:40:11 +0800

    add some code

--- Chapter2/2.4 httpx的使用/4.Client对象.py
+++ Chapter2/2.4 httpx的使用/4.Client对象.py
@@ -6,8 +6,29 @@
 
 
 # P77 Client 对象
+# 推荐的写法
+# import httpx
+#
+# with httpx.Client() as client:
+#     response = client.get('https://httpbin.org/get')
+#     print(response)
+
+# 上下两种写法是等价的，结果是一样的
+
+# client = httpx.Client()
+# try:
+#     response = client.get('https://httpbin.org/get')
+# finally:
+#     client.close()
 
 
+# 同样可以携带 headers 参数
+import httpx
 
+url = 'http://httpbin.org/headers'
+headers = {'User-Agent': 'my-app/0.0.1'}
+with httpx.Client(headers=headers) as client:
+    r = client.get(url)
+    print(r.json()['headers']['User-Agent'])
 
 
