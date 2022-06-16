3b699986699a52646bf6deaef236ff46fdc0c8f4
Lucidity <2993514952@qq.com>
2022-06-16 17:28:20 +0800

    add some code

--- Chapter2/2.4 httpx的使用/3.基本使用.py
+++ Chapter2/2.4 httpx的使用/3.基本使用.py
@@ -5,12 +5,21 @@
 # @Project : Spider
 
 
-# P75
+# P75 httpx 的基本使用
+# import httpx
+#
+# response = httpx.get('https://httpbin.org/get')
+# print(response.status_code)
+# print(response.headers)
+# print(response.text)
+
+
+# 下面换一个 User-Agent 再请求一次
 import httpx
 
-response = httpx.get('https://httpbin.org/get')
-print(response.status_code)
-print(response.headers)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
+}
+response = httpx.get('https://httpbin.org/get', headers=headers)
+
 print(response.text)
-
-
