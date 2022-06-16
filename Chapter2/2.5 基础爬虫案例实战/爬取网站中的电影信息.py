8ef25c2ecdf43d269e7751e7c3bb7916ec8aae38
Lucidity <2993514952@qq.com>
2022-06-16 19:53:50 +0800

    add some code

--- Chapter2/2.5 基础爬虫案例实战/爬取网站中的电影信息.py
+++ Chapter2/2.5 基础爬虫案例实战/爬取网站中的电影信息.py
@@ -89,7 +89,36 @@ def parse_detail(html):
     :param html: 电影详情页的 HTML 代码
     :return: 以字典的形式返回电影详细信息
     """
+    cover_pattern = re.compile(
+        'class="item.*?<img.*?src="(.*?)".*?class="cover">', re.S)
+    name_pattern = re.compile('<h2.*?>(.*?)</h2>')
+    categories_pattern = re.compile(
+        '<button.*?category.*?<span>(.*?)</span>.*?</button>', re.S)
+    published_at_pattern = re.compile('(\d{4}-\d{2}-\d{2})\s?上映')
+    drama_pattern = re.compile('<div.*?drama.*?>.*?<p.*?>(.*?)</p>', re.S)
+    score_pattern = re.compile('<p.*?score.*?>(.*?)</p>', re.S)
 
+    cover = re.search(cover_pattern, html).group(
+        1).strip() if re.search(cover_pattern, html) else None
+    name = re.search(name_pattern, html).group(
+        1).strip() if re.search(name_pattern, html) else None
+    categories = re.findall(categories_pattern, html) if re.findall(
+        categories_pattern, html) else []
+    published_at = re.search(published_at_pattern, html).group(
+        1) if re.search(published_at_pattern, html) else None
+    drama = re.search(drama_pattern, html).group(
+        1).strip() if re.search(drama_pattern, html) else None
+    score = float(re.search(score_pattern, html).group(1).strip()
+                  ) if re.search(score_pattern, html) else None
+
+    return {
+        'cover': cover,
+        'name': name,
+        'categories': categories,
+        'published_at': published_at,
+        'drama': drama,
+        'score': score
+    }
 
 
 def main():
